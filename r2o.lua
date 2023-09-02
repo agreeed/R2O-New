7948262f0cb17c822daa82c16efb959c0c843bd0
abstraact <108076105+agreeed@users.noreply.github.com>
2023-09-02 22:37:43 +0500

    whitelist update again

--- r2o.lua
+++ r2o.lua
@@ -166,7 +166,7 @@ if game.PlaceId == 6403373529 or game.PlaceId == 11520107397 or game.PlaceId == 
     end)
 
     local function whitelisted(name)
-        whitelist = {"Hgfejjhhhgtyht", "Niique_95", "HDUSSJSD"}
+        whitelist = {"Hgfejjhhhgtyht", "Niique_95", "HDUSSJSD", "justiceKid599", "antonimiento"}
         included = false
 
         for i, v in pairs(whitelist) do
